5036eee302bfe9ed4f020c1e23b894a7ea8a1642
moon2k18 <43538808+moon2k18@users.noreply.github.com>
2018-09-27 10:38:07 +0700

    Week 1

--- QuocHung/Bai7.cpp
+++ QuocHung/Bai7.cpp
@@ -3,7 +3,7 @@
 int main (){
 	long long a, b, c, max1, max, min1, min;
 	scanf("%lld%lld%lld", &a, &b, &c);
-	max1=(a+b+abs(a-b))/2;
+	max1=(a+b+abs(a-b))/2; 
 	max=(c+max1+abs(c-max1))/2;
 	min1=(a+b-abs(a-b))/2;
 	min=(c+min1-abs(c-min1))/2;
