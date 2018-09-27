e3d0fab3fe2002f20f3f604db29cab64c1fa0164
moon2k18 <43538808+moon2k18@users.noreply.github.com>
2018-09-27 10:37:21 +0700

    Update Bai7.cpp

--- QuocHung/Bai7.cpp
+++ QuocHung/Bai7.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main (){
-	long long a, b, c, max1, max;
+	long long a, b, c, max1, max, min1, min;
 	scanf("%lld%lld%lld", &a, &b, &c);
 	max1=(a+b+abs(a-b))/2;
 	max=(c+max1+abs(c-max1))/2;
-	printf("%lld",max);
+	min1=(a+b-abs(a-b))/2;
+	min=(c+min1-abs(c-min1))/2;
+	printf("%lld %lld", max, min);
 }
