d514f53d0f38c49e57e26ad8838f2c24ea2b9b8b
yeu ha noi <43584342+healer7599@users.noreply.github.com>
2018-10-07 00:25:34 +0700

    Tuần 2

--- PhanNguyet/câu1.cpp
+++ PhanNguyet/câu1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
 	int a,b,c,max,min;		// chỉnh lại bố cục code
-	scanf("%d %d %d",&a,&b,&c);	// trong scanf hạn chế thêm các kí tự khaonrg trắng
+	scanf("%d %d %d",&a,&b,&c);	// trong scanf hạn chế thêm các kí tự kh trắng
 	if (a>=b && a>=c) {		
 		max=a;
 		}
