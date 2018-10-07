ed2051bcb86cb97d4fdfc4f5a3956024d68f7e2e
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-07 10:52:26 +0700

    Update câu3.cpp

--- PhanNguyet/câu3.cpp
+++ PhanNguyet/câu3.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main (){
-	int n;
-	scanf("%d",&n);		// chỉnh lại bố cục code
-	if (n==0){
-		printf("0");
-		}
-	else if (n<0){ 
-	printf("so am");
-	}
-	else printf ("so duong");
+ int n;
+ scanf("%d",&n);		// chỉnh lại bố cục code
+   if (n==0){
+      printf("0");
+   }
+   else if (n<0){ 
+      printf("so am");
+   }
+   else printf ("so duong");
 return 0;
 }		
