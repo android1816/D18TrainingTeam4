876ba64022bcedef214c5d95684a20682198494e
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-07 10:55:24 +0700

    Update câu4.cpp

--- PhanNguyet/câu4.cpp
+++ PhanNguyet/câu4.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
-int main (){
-	long long n; float t;
-	scanf("%ld",&n);
+ int main (){
+ long long n; float t;
+ scanf("%ld",&n);
     if (n>1000000){
 	if (n>1000000000){
 		t=n/2;
 		}
 	else if (n>15000000 && n<1000000000) {
-		t=n-n/3;	// sai toán học
+		t=n-3*n/10;
 		}
 	else if (n>7000000 && n<15000000)  {
 	    t=n-n/5;
@@ -15,5 +15,5 @@ int main (){
 	else t=n-n/10;
 	printf("%f",t);	  	 
 	}
-return 0;
+ return 0;
 }	
