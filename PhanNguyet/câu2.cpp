a6ad9422c74290e16050300bcf23cb036753635f
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-07 10:49:23 +0700

    Update câu2.cpp

--- PhanNguyet/câu2.cpp
+++ PhanNguyet/câu2.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main (){
-	int n;
-	scanf("%d",&n);
-	if (n>0){	// sai logic, sửa lỗi  và chỉnh lại bố cục code
-	if (n%2==0){
-		printf("so chan");
-		}
+  int n;
+  scanf("%d",&n);
+       if (n%2==0){
+          printf("so chan");
+	  }
 	else printf("so le");
-	}
 return 0;
 }		
