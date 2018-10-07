773423369fbd8986f9ae0b0012ddd23b3bec7043
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-07 11:02:07 +0700

    Update câu5.cpp

--- PhanNguyet/câu5.cpp
+++ PhanNguyet/câu5.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 int main (){
-	int a,b,c;
-	scanf("%d %d %d",&a,&b,&c);
-	if ((a>0) &&(b>0)&&(c>0)) {
-		if ((a+b>c)&&(b+c>a)&&(c+a>b)) {	// cần phải tính diện tích tam giác
-		printf("la 3 canh tam giac");
-		}
-	else printf("khong la 3 canh tam giac");
-	}
+ int a,b,c; 
+ float p,s;
+ scanf("%d %d %d",&a,&b,&c);
+  if ((a>0) &&(b>0)&&(c>0)) {
+     if ((a+b>c)&&(b+c>a)&&(c+a>b)) {
+	 printf("la 3 canh tam giac\n");
+	 p=(a+b+c)/2;
+	 s=sqrt(p*(p-a)*(p-b)*(p-c));
+	 printf("dien tich tam giac la: %.2f",s);
+      }
+   else printf("khong la 3 canh tam giac");
+  }
 return 0;
 }	
