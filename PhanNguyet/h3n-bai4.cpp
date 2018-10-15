0202b71b78f520abd4a1e5702503a71177560ff5
yeu ha noi <43584342+healer7599@users.noreply.github.com>
2018-10-15 17:14:53 +0700

    need change

--- PhanNguyet/h3n-bai4.cpp
+++ PhanNguyet/h3n-bai4.cpp
@@ -3,7 +3,8 @@ int main(){
 	int n,k;
 	scanf("%d",&n);
 	k=n-1;
-	for (int i=0; i<n-1; i++) {
+	for (int i=0; i<n-1; i++) { 	// bài 4, 5 cần chỉnh lại ý tưởng, chỉ cần in khoảng trắng trước khi in số hoặc * thôi
+					// còn sau khi in xong số, * thì không cần in khoảng trắng nữa	
 		for (int j=0; j<2*n-1; j++) {
 			if (j>=k-i && j<=k+i) printf("*");
 			else printf(" ");
