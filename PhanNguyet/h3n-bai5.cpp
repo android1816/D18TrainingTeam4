3f998f6d23effe16df13a8d25dd2f126e2081e3e
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-15 21:54:15 +0700

    Update h3n-bai5.cpp

--- PhanNguyet/h3n-bai5.cpp
+++ PhanNguyet/h3n-bai5.cpp
@@ -4,17 +4,17 @@ int main() {
 	scanf("%d",&n);
 	k=n-1;
 	for (int i=0; i<n; i++) {
-		for (int j=0; j<2*n-1; j++) {
+		for (int j=0; j<=k+i; j++) {
 			if (j>=k-i && j<k) printf("%d",j+1-(k-i));
-			else if (j>=k && j<=k+i) printf("%d",k+i+1-j);
+			else if (j>=k) printf("%d",k+i+1-j);
 			else printf(" ");
 		}
 		printf ("\n");
 	}
 	for (int i=n-2; i>=0; i--) {
-		for (int j=0; j<2*n-1;j++) {
+		for (int j=0; j<=k+i;j++) {
 			if (j>=k-i && j<k) printf("%d",j+1-(k-i));
-			else if (j>=k && j<=k+i) printf("%d",k+i+1-j);
+			else if (j>=k) printf("%d",k+i+1-j);
 			else printf(" ");
 		}
 		printf("\n");
