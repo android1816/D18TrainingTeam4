3c708187e07bab569a4c8f11a3176f5b8c383008
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-15 21:47:59 +0700

    Update h3n-bai4.cpp

--- PhanNguyet/h3n-bai4.cpp
+++ PhanNguyet/h3n-bai4.cpp
@@ -5,19 +5,19 @@ int main(){
 	k=n-1;
 	for (int i=0; i<n-1; i++) { 	// bài 4, 5 cần chỉnh lại ý tưởng, chỉ cần in khoảng trắng trước khi in số hoặc * thôi
 					// còn sau khi in xong số, * thì không cần in khoảng trắng nữa	
-		for (int j=0; j<2*n-1; j++) {
-			if (j>=k-i && j<=k+i) printf("*");
+		for (int j=0; j<=k+i; j++) {
+			if (j>=k-i) printf("*");
 			else printf(" ");
 		}
 		printf("\n");
 	}
-		for (int i=1; i<2*n;i++) {
+	for (int i=1; i<2*n;i++) {
 	    printf("*");
-	    }
+	}
 	printf("\n");
 	for (int i=n-2; i>=0; i--) {
-		for (int j=0; j<2*n-1; j++) {
-			if (j>=k-i && j<=k+i) printf("*");
+		for (int j=0; j<=k+i; j++) {
+			if (j>=k-i) printf("*");
 			else printf(" ");
 		}
 		printf("\n");
