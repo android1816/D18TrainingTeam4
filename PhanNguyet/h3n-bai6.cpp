4aad13fd7e92922b954d47638d437bba0da41670
yeu ha noi <43584342+healer7599@users.noreply.github.com>
2018-10-15 17:11:58 +0700

    need change

--- PhanNguyet/h3n-bai6.cpp
+++ PhanNguyet/h3n-bai6.cpp
@@ -2,7 +2,7 @@
 int main() {
 	int a,b,d;
 	scanf("%d %d",&a,&b);
-	if (a<=b){
+	if (a<=b){	// dựa theo ý tưởng code trong else, em chỉnh lại bài này chỉ dùng 2 for lồng nhau 
 	  for (int i=0; i<a; i++) {
 		for (int j=i+1; j<=b; j++){
 		    printf("%d", j);
