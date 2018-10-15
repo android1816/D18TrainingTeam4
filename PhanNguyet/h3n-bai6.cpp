fc029d786cab3833902b50fffddcba3dda645863
nguyetphanthi <43547824+nguyetphanthi@users.noreply.github.com>
2018-10-15 22:59:58 +0700

    Update h3n-bai6.cpp

--- PhanNguyet/h3n-bai6.cpp
+++ PhanNguyet/h3n-bai6.cpp
@@ -2,13 +2,13 @@
 int main() {
 	int a,b,d;
 	scanf("%d %d",&a,&b);
-	if (a<=b){	// dựa theo ý tưởng code trong else, em chỉnh lại bài này chỉ dùng 2 for lồng nhau 
-	  for (int i=0; i<a; i++) {
-		for (int j=i+1; j<=b; j++){
-		    printf("%d", j);
-		}
-		for (int j=1; j<=i; j++ ) {
-		    printf("%d",j);
+	if (a<=b){
+	    for (int i=0; i<a; i++) {
+		for (int j=i+1; j<=b+i; j++){
+			if (j<=b) 
+			  d=j;
+			else d=j-b;
+		    printf("%d",d);
 		}
 		printf("\n");
 	  }
