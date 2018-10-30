5cd52243e4fe5ba99fe0e85e99e09b3a383833fe
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 00:40:55 +0700

    Bài tập anh Tuấn -.-

---
+++ MinhDuc/h1n--bai5.cpp
@@ -0,0 +1,19 @@
+//Code By PMD
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+	double a, b, c, p, s;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	if( a+b>c && b+c>a && a+c>b )
+	{
+		p=(a+b+c)/2;
+		s=sqrt(p*(p-a)*(p-b)*(p-c));
+		printf("%lf", s);
+	}
+	else printf("-1");
+    return 0;
+}
+//Code By PMD
+
+
