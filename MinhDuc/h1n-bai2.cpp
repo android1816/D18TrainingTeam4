5cefed0d6e2a04e808bf3e386cc00c7f3db31067
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 00:37:08 +0700

    Bài tập anh Tuấn :v

---
+++ MinhDuc/h1n-bai2.cpp
@@ -0,0 +1,17 @@
+//Code By PMD
+#include<stdio.h>
+
+int main ()
+{
+
+    long long a;
+	  scanf("%lld", &a);
+	if(a%2==0) 
+	  printf("even");
+	else 
+	  printf("odd"); 
+    return 0;
+}
+//Code By PMD
+
+
