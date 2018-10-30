07d1d874bb4fb74350e7787eef28fa139d5e9d6f
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 00:39:01 +0700

    Bài tập anh Tuấn :v

---
+++ MinhDuc/h1n-bai3.cpp
@@ -0,0 +1,18 @@
+//Code By PMD
+#include<stdio.h>
+
+int main ()
+{
+  long long a;
+	  scanf("%lld", &a);
+	if(a<0) 
+	  printf("am");
+	if(a>0) 
+	  printf("duong");
+	if(a==0) 
+	  printf("khong la ca hai");
+  return 0;
+}
+//Code By PMD
+
+
