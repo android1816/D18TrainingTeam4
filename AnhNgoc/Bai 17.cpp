07c1b4d95b88f9f884a48240ab576080bb7dd036
ninhngocnhi <43540391+ninhngocnhi@users.noreply.github.com>
2018-10-31 23:46:32 +0700

    htn-bai7 sua

--- AnhNgoc/Bai 17.cpp
+++ AnhNgoc/Bai 17.cpp
@@ -4,41 +4,42 @@ int main() {
 	long long a;
 	printf ("nhap tao xem nao: ");
 	scanf ("%lld", &a);
-	if (a==0){
-		printf ("zero");
-	}
-	if (a==1){
-		printf ("one");
-	}
-	if (a==2){
-		printf ("two");
-	}  
-	if (a==3){
-		printf ("three");
-	}
-	if (a==4){
-		printf ("four");
-	}
-	if (a==5){
-		printf ("five");
-	}
-	if (a==6){
-		printf ("six");
-	}
-	if (a==7){
-		printf ("seven");
-	}
-	if (a==8){
-		printf ("eight");
-	}
-	if (a==9){
-		printf ("nine");
-	}
-	if (a==10){
-		printf ("ten");
-	}
-	if (a>10){
-		printf ("greater than ten");
+	switch (a) {
+		case 0:
+			printf ("zero");
+			break;
+		case 1:
+			printf ("one");
+			break;
+		case 2:
+			printf ("two");
+			break;
+		case 3:
+			printf ("three");
+			break;
+		case 4:
+			printf ("four");
+			break;
+		case 5:
+			printf ("five");
+			break;
+		case 6:
+			printf ("six");
+			break;
+		case 7:
+			printf ("seven");
+			break;
+		case 8:
+			printf ("eight");
+			break;
+		case 9:
+			printf ("nine");
+			break;
+		case 10:
+			printf ("ten");
+			break;
+		default:
+			printf ("greater than ten");
 	}
 	return 0;
 }
