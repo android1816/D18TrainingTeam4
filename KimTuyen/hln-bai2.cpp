25ee8ce7b88d70ece5deaee54b2b5d35e8f70a29
TuyenVy911 <43547534+TuyenVy911@users.noreply.github.com>
2018-10-31 13:10:02 +0700

    Tuần 2

---
+++ KimTuyen/hln-bai2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    long long a;
+	printf("Nhap so nguyen a:");
+	scanf("%lld", &a);
+	    if(a%2==0)
+		printf("even");
+		else 
+		printf("odd");
+    return 0;
+}
+
