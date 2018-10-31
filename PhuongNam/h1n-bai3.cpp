6697cd995d1428be9bc0e1a8250a49d3447d5f8d
namvu123 <43583158+namvu123@users.noreply.github.com>
2018-10-31 12:13:34 +0700

    Tuần 3

---
+++ PhuongNam/h1n-bai3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	float a;
+	printf("nhap so a:");
+	scanf("%f",&a);
+	if(a<0)
+	{
+		printf("am");
+		}
+	if(a>0)
+	{
+		printf("duong");
+		}
+	else
+	{
+		printf("khong la ca hai");
+		}
+	}
