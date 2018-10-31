aa172edf47ac530f697bfe2cf7ca0c3c4401051a
trungkzqh1 <43533531+trungkzqh1@users.noreply.github.com>
2018-10-31 10:14:47 +0700

    bài tập if và else

---
+++ BaoTrung/h1n-bai3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("nhap so N: ");
+	scanf("%d",&a);
+	if (a>0) {
+		printf("duong");
+	}
+	else if (a<0) {
+		printf("am");
+	}
+	else {
+		printf("ko la ca hai");
+	}
+}
+
+
+
