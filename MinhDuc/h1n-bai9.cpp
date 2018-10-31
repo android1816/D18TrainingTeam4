c6b55aac9f13b6540555f67f56211664bb73864c
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 21:20:58 +0700

    Bài tập anh Tuấn

--- MinhDuc/h1n-bai9.cpp
+++ MinhDuc/h1n-bai9.cpp
@@ -1,21 +1,23 @@
 //Code By PMD
 #include<stdio.h>
-#include<math.h>
+
 int main ()
 {
-	int a,b,c,d,e,f;
-	float x,y;
+    long long a,b,c,d,e,f;
+    float x,y;
        printf ("nhap gia tri a b c d e f : ");
-       scanf ("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+       scanf ("%lld%lld%lld%lld%lld%lld",&a,&b,&c,&d,&e,&f);
        y=(float)(f*a-d*c)/(a*e-b*d);
-       x=(float)(c-b*y)/a;
+       x=(float)(c-b*y)/a;    
+    if (0<=a && 0<=b && 0<=c) 
+{
     if (a/d==b/e && b/e!=c/f ) printf ("vo nghiem");
     else
-    if (a/d==b/e && b/e==c/f ) printf (" vo so nghiem");
+    if (a/d==b/e && b/e==c/f ) printf ("vo so nghiem");
     else
-	    printf ("nghiem cua he la (%.5f , %.5f) ",x,y);
-	return 0;	 
-
+	   printf ("nghiem cua he la (%.5f , %.5f) ",x,y);
+}
+    return 0;	 
 
 }
 //Code By PMD
