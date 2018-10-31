5525d0407b2d2d7a3680b54df611472faac0817a
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 20:07:13 +0700

    h1n-bai7

--- MinhDuc/h1n-bai7.cpp
+++ MinhDuc/h1n-bai7.cpp
@@ -3,24 +3,30 @@
 
 int main ()
 {
-	int a;
-	    printf ("nhap gia tri a : ");
-	    scanf("%d",&a);
-	     if (a==0) {printf ("zero");}
-	else if (a==1) {printf ("one");}
-    else if (a==2) {printf ("two");}
-	else if (a==3) {printf ("three");}
-	else if (a==4) {printf ("four");}
-	else if (a==5) {printf ("five");}
-	else if (a==6) {printf ("six");}
-	else if (a==7) {printf ("seven");}
-    else if (a==8) {printf ("eight");}
-	else if (a==9) {printf ("nine");}
-	else if (a==10){printf ("ten");}
-	else if (a>10) {printf ("greater than ten");}
-	
-	return 0;
 
+int a;
+	printf("nhap vao so a:");
+	scanf("%d",&a);
+	{
+		switch (a)
+		{
+			case 0 : printf("zero");break;
+			case 1 : printf("one");break;
+			case 2 : printf("two");break;
+			case 3 : printf("three");break;
+			case 4 : printf("four");break;
+			case 5 : printf("five");break;
+			case 6 : printf("six");break;
+			case 7 : printf("seven");break;
+			case 8 : printf("eight");break;
+			case 9 : printf("nine");break;
+			case 10: printf("ten");break;
+			default: printf("greater than ten");
+		}	   
+	}
+
+return 0;
 }
 //Code By PMD
+
 
