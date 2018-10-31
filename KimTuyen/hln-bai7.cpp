084ad90861827ec40944aeb2c4f6781657b24d8f
TuyenVy911 <43547534+TuyenVy911@users.noreply.github.com>
2018-10-31 16:30:55 +0700

    Tuần 2

--- KimTuyen/hln-bai7.cpp
+++ KimTuyen/hln-bai7.cpp
@@ -7,7 +7,7 @@ int main()
 	{
 		case 0: printf("zero\n");
 		        break;
-	    case 1: printf("one\n");
+	        case 1: printf("one\n");
 		        break;
 		case 2: printf("two\n");
 		        break;
