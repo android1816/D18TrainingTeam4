b12beb74c7bd49d05be52d85ba45b3853b7b4fad
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 20:33:48 +0700

    Bài tập anh Tuấn

--- MinhDuc/h1n-bai5.cpp
+++ MinhDuc/h1n-bai5.cpp
@@ -3,18 +3,20 @@
 #include<math.h>
 int main ()
 {
-	double a, b, c, p, s;
-	scanf("%lf%lf%lf", &a, &b, &c);
+    long int a, b, c ;
+    float p,s ;
+	scanf("%d%d%d", &a, &b, &c);
 	if( a+b>c && b+c>a && a+c>b )
 	{
-		p=(a+b+c)/2;
-		s=sqrt(p*(p-a)*(p-b)*(p-c));
-		printf("%lf", s);
+		p=(float)(a+b+c)/2;
+		s=(float)sqrt(p*(p-a)*(p-b)*(p-c));
+		printf("%f", s);
 	}
 	else 
 		printf("-1");
         return 0;
 }
 //Code By PMD
+
 
 
