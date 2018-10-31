4528a7516da774a688aeed8047b42901f31615d3
TuyenVy911 <43547534+TuyenVy911@users.noreply.github.com>
2018-10-31 17:39:59 +0700

    Tuần 2

--- KimTuyen/hln-bai5.cpp
+++ KimTuyen/hln-bai5.cpp
@@ -5,7 +5,7 @@ int main()
 	int a,b,c,p;
 	float S;
 	scanf("%d%d%d", &a,&b,&c);
-    if(((a+b)>c) && ((a+c)>b) && ((b+c)>a))
+    if(((a+b)>c) && ((a+c)>b) && ((b+c)>a) && (a>0) && (b>0) && (c>0))
 	{ printf("La mot tam giac \n");
     p=(a+b+c)/2;
 	S=sqrt(p*(p-a)*(p-b)*(p-c));	
