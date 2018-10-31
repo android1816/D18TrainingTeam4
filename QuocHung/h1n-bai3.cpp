cf1757f14f6fee3684e02d52c781c1740280c646
moon2k18 <43538808+moon2k18@users.noreply.github.com>
2018-10-31 12:34:55 +0700

    h1n-bai3

--- QuocHung/h1n-bai3.cpp
+++ QuocHung/h1n-bai3.cpp
@@ -3,6 +3,6 @@ int main(){
 	long long a;
 	scanf("%lld", &a);
 	if(a<0) printf("am");
-	if(a>0) printf("duong");
-	if(a==0) printf("khong la ca hai");
+	else if(a>0) printf("duong");
+	else printf("khong la ca hai");
 }
