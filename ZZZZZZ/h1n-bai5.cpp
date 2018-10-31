fefa32b26f53029e6f1ef277abd4a53202f9882c
manh610 <43571636+manh610@users.noreply.github.com>
2018-10-31 11:05:30 +0700

    h1n

--- ZZZZZZ/h1n-bai5.cpp
+++ ZZZZZZ/h1n-bai5.cpp
@@ -2,10 +2,10 @@
 #include <math.h>
 int main()
 {
-	unsigned int a,b,c,p;
+	unsigned int a,b,c;
 	printf ("moi ban nhap do dai 3 canh: ");
 	scanf ("%d %d %d", &a, &b, &c);
-	p=(a+b+c)/2;
+	float p=(a+b+c)/2;
 	if ( (a+b>c) && (a+c>b) && (b+c>a) )
 		{
 			printf ("la 3 canh cua tam giac\n");
