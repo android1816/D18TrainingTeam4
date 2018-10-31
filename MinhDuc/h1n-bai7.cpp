1a171d13bff76b79ef6ed28eabc94c7fb73ddcd9
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-10-31 11:03:44 +0700

    Bài tập anh Tuấn...

---
+++ MinhDuc/h1n-bai7.cpp
@@ -0,0 +1,26 @@
+//Code By PMD
+#include<stdio.h>
+
+int main ()
+{
+	int a;
+	    printf ("nhap gia tri a : ");
+	    scanf("%d",&a);
+	     if (a==0) {printf ("zero");}
+	else if (a==1) {printf ("one");}
+    else if (a==2) {printf ("two");}
+	else if (a==3) {printf ("three");}
+	else if (a==4) {printf ("four");}
+	else if (a==5) {printf ("five");}
+	else if (a==6) {printf ("six");}
+	else if (a==7) {printf ("seven");}
+    else if (a==8) {printf ("eight");}
+	else if (a==9) {printf ("nine");}
+	else if (a==10){printf ("ten");}
+	else if (a>10) {printf ("greater than ten");}
+	
+	return 0;
+
+}
+//Code By PMD
+
