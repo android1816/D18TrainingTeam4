c67eb393a8c3a5d9aa5f18794efae6f38d11a95a
Luận Phạm <43569382+ladypew@users.noreply.github.com>
2018-10-31 21:16:30 +0700

    Add files via upload

---
+++ Luan/PHAMLUAN/h1n-bai2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+	int main()
+	{
+		long long a;
+		printf("nhap so a");
+		scanf("%lld", &a);
+		if (a%2==0)
+		{
+			printf("even");
+		}
+		else{
+			printf("odd");
+		}
+	return 0;
+	}
+
+
