bcff0d00acefb493eec61b5ddf64e42f6d068a35
trungkzqh1 <43533531+trungkzqh1@users.noreply.github.com>
2018-11-07 23:58:17 +0700

    vòng lặp và mảng

---
+++ BaoTrung/h2n-bai6.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+  int a,b,n;
+  scanf("%d",&n);
+  a=0;
+  if (n>0){
+  for (int i=1 ;i<n;i++){
+    if (n%i==0) 
+  	a=a+i;
+  }
+  if (a==n){
+  for (int i=1;i<n;i++){
+  	if (n%i==0) 
+  	printf ("%d ",i);
+  	}
+  }
+   if (a!=n) printf ("-1");
+	}
+}
