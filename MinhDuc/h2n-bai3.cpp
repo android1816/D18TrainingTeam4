b22e0f770a36928182cfca176f58376b3ed44282
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-11-08 11:20:11 +0700

    h2n

---
+++ MinhDuc/h2n-bai3.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, i, j, s,d=1;
+	scanf("%d %d",&a,&b);
+	if (a>b) return 0;
+	for(i=a; i<=b; ++i)
+{
+	s=1;
+	for(j=2; j<=i/2; ++j)
+	{
+		if(i%j==0)
+		{
+			s=0;
+			break;
+		}
+	}
+		if(s==1)
+		{
+		d=0;
+		printf("%d ",i);
+	    }
+}
+	if (d==1) printf ("-1");
+     return 0;
+}
