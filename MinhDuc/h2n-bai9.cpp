6ce92d9311e17c3348d4915db70b5af9221774ee
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-11-10 00:13:08 +0700

    h2n

--- MinhDuc/h2n-bai9.cpp
+++ MinhDuc/h2n-bai9.cpp
@@ -1,33 +1,26 @@
 #include <stdio.h>
 int main()
 {
-  ;
+  
     int n,i,j; 
-    long long x,a[100000];
-        scanf("%d %lld",&n,&x);
+    long long x,a[100];
+        scanf("%lld %lld",&n,&x);
     for(i = 0; i < n; i++)
     {
 		scanf("%lld",&a[i]);
 	}
-	for(i=1;i<=n;)
-	{
-		if(a[i]==x)
-		{
-			j=i;
-			for(i=j;i<=n;i++)
-			{
-				a[i]=a[i+1];
-			}
-			n--;
-			i=j;
-		}
-		else i++;
-		
-}
+     for (i=n;i>=0;i--)                             
+    {
+        if (a[i] == x)                            
+        {
+            for (j = i; j < n ; j++)             
+                a[j] = a[j + 1];
+            n--;
+        }                                  
+    }
     for (i = 0; i < n; i++) 
     {
 	 	printf("%lld ", a[i]);
 	}
 	return 0;
 }
-
