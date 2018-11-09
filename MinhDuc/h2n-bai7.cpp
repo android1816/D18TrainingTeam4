0d2aa1b1feacca91b1d26dcca0c0e015dbb09344
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-11-10 00:05:40 +0700

    h2n

--- MinhDuc/h2n-bai7.cpp
+++ MinhDuc/h2n-bai7.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#define max 100000
 int main()
 {
     int n,k;
-    long long a,A[100000];
+    long long a,A[100];
     scanf("%d %d %lld",&n,&k,&a);
-    if (k>n || n>max || k<=0) return 0;
     for(int i=0; i<n; i++)
     {
         scanf("%d",&A[i]);
@@ -13,12 +11,12 @@ int main()
     for (int i=n;i>=k;i--)
 	{
     	A[i]=A[i-1];
-	}
-	A[k]=a;
-	n=n+1;
-	for(int i=0; i<n; i++)
+	if( i==k ) A[i-1] = a;
+    }
+	for(int i=0; i<=n; i++)
     {   
         printf("%d ", A[i]);
     }
     return 0;
 }
+    
