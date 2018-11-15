8d0a067217d87b589b59353b151bf2bee4961c3b
sonnhthtb <43534240+sonnhthtb@users.noreply.github.com>
2018-11-15 10:44:07 +0700

    Add files via upload

---
+++ QuangHuy/h2n-bai11.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+	int n, m, i, j,a[100000], b[100000], c;
+	scanf ("%d%d", &n, &m);
+	for (i=0;i<n;i++)
+	{
+		scanf ("%d", &a[i]);
+	}
+	for (i=0;i<m;i++)
+	{
+		scanf ("%d", &b[i]);
+	}
+	sort(a,a+n);
+	sort(b,b+m);
+	for (i=0;i<n;i++)
+	{
+		for (j=0;j<m;j++)
+		{
+			if (b[j]<a[i])
+			{
+				c=b[j];
+				b[j]=a[i];
+				a[i]=c;
+			}
+		}
+	}
+	sort(a,a+n);
+	sort(b,b+m);
+	for (i=0;i<n;i++)
+	{
+		printf ("%d ", a[i]);
+	}
+	for (i=0;i<m;i++)
+	{
+		printf ("%d ", b[i]);
+	}
+}
