cb68080131fe60dbc7247d1b3ced7d76f4442577
ndoanh266 <43532403+ndoanh266@users.noreply.github.com>
2018-11-15 12:24:12 +0700

    mang

---
+++ TheDoanh/h2n-bai11.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,m,i,j,k;
+	scanf("%d%d",&n,&m);
+	int a[n+5],b[m+5];
+	
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	
+	for (i=0;i<m;i++){
+		scanf("%d",&b[i]);
+	}
+	
+	sort(a,a+n);
+	sort(b,b+m);
+	
+	for (i=m-1;i>=0;i--)
+	{
+		n++;
+		a[n-1]=b[i];
+	}
+	
+	sort(a,a+n);
+	
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+	
+}
