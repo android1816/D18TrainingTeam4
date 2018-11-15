0e30ee1f9e269de20ac1fd455edd5d71fb322b5c
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-11-15 13:37:54 +0700

    h2n

--- MinhDuc/h2n-bai11.cpp
+++ MinhDuc/h2n-bai11.cpp
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-	int n,m,i,j,a[100000],b[100000],k;
+	int n,m,i,j,k;
 	scanf("%d%d",&n,&m);
+	int a[100005],b[100005];	
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
 	for (i=0;i<m;i++){
 		scanf("%d",&b[i]);
 	}
-	sort(a,a+n)	;sort(b,b+m);
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-			if(b[j]<a[i]){
-				k=b[j];
-				b[j]=a[i];
-				a[i]=k;
-			}
-		}
-	}
+	sort(a,a+n);
 	sort(b,b+m);
+	for (i=m-1;i>=0;i--)
+	{
+		n++;
+		a[n-1]=b[i];
+	}
+	sort(a,a+n);
 	for(i=0;i<n;i++){
 		printf("%d ",a[i]);
-	}
-	for (i=0;i<m;i++){
-		printf("%d ",b[i]);
-	}
+	}	
 }
-
-
