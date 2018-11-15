2ae0af33d3c937817eca498aee30b7068dd72448
ducvipga2000 <43582788+ducvipga2000@users.noreply.github.com>
2018-11-15 11:17:20 +0700

    h2n

--- MinhDuc/h2n-bai10.cpp
+++ MinhDuc/h2n-bai10.cpp
@@ -7,9 +7,9 @@ int main()
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
-	for(i=n;i>1;i--){
-		for(j=0;j<i;j++){
-			if(a[j]<a[j+1]){
+	for(i=0;i<n-1;i++){
+		for(j = 0;j<n-i-1; j++){
+			if(a [j] < a[j+1] ){
 				b=a[j+1];
 				a[j+1]=a[j];
 				a[j]=b;
