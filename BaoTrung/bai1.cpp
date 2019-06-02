9a2cf14d2a378e9529f2ed26cb011998fc9da62c
trungkzqh1 <43533531+trungkzqh1@users.noreply.github.com>
2019-06-02 15:17:09 +0700

    kt

---
+++ BaoTrung/bai1.cpp
@@ -0,0 +1,15 @@
+/*
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+main(){
+	int a,b,c;
+	scanf("%d + %d = %d",&a,&b,&c);
+	if(a+b==c) printf("YES");
+	else printf("NO"); 
+}
+
+// code by Bao Trung NTP
+
